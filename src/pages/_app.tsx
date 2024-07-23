098de7914e88df91b51c8ae4bb0cb04e33a40a6f
Spenser Bushey <spenser@spenserbushey.com>
2024-07-22 21:02:31 -0700

    install vercel speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { GeistSans } from "geist/font/sans";
 import { type AppType } from "next/app";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { api } from "~/utils/api";
 
@@ -10,6 +11,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <div className={GeistSans.className}>
       <Analytics />
+      <SpeedInsights />
       <Component {...pageProps} />
     </div>
   );
