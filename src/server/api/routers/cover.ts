c876221c677a34d6b2345b19becd426f7faa7313
Spenser Bushey <spenser@spenserbushey.com>
2024-07-26 10:57:39 -0700

    Fix sending empty requests to replicate

--- src/server/api/routers/cover.ts
+++ src/server/api/routers/cover.ts
@@ -9,11 +9,12 @@ interface ClipReturnObject {
 
 export const coverRouter = createTRPCRouter({
   getTextEmbedding: publicProcedure
-    .input(z.string())
+    .input(z.string().trim().min(1))
     .query(async ({ input }) => {
       const replicate = new Replicate({
         auth: process.env.REPLICATE_API_TOKEN,
       });
+      console.log(`Running replicate with ${input}`);
       const [output] = (await replicate.run(
         "andreasjansson/clip-features:75b33f253f7714a281ad3e9b28f63e3232d583716ef6718f2e46641077ea040a",
         {
