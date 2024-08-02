81902c037a9c6c43552fb126cbc37c9fa2323ea8
Spenser Bushey <spenser@spenserbushey.com>
2024-08-01 17:17:49 -0700

    Change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 
-import { GeistSans } from "geist/font/sans";
+import { Gemunu_Libre } from "next/font/google";
 import { type Metadata } from "next";
 
 import { TRPCReactProvider } from "@/trpc/react";
@@ -9,6 +9,7 @@ import Link from "next/link";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { GoogleAdsense } from "@/components/GoogleAdsense";
 import { env } from "@/env";
+import React from "react";
 
 export const metadata: Metadata = {
   title: "Audiobook Covers",
@@ -16,37 +17,48 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+const gemunu = Gemunu_Libre({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
+    <html lang="en" className={gemunu.className}>
       <body>
         <TRPCReactProvider>
           <div className="flex min-h-screen flex-col justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-            <div className="my-6 flex flex-row justify-center gap-8">
-              <Link className="my-auto" href="/">
-                <span className="text-2xl">Home</span>
-              </Link>
+            <div className="my-6 flex flex-row justify-center gap-2">
+              <Button asChild variant="ghost">
+                <Link href="/" className="text-3xl">
+                  Home
+                </Link>
+              </Button>
 
               <Button asChild variant="ghost">
-                <Link href="/image/search/" className="text-l">
+                <Link href="/image/search/" className="text-3xl">
                   Search
                 </Link>
               </Button>
 
               <Button asChild variant="ghost">
-                <Link href="/about/" className="text-l">
+                <Link href="/about/" className="text-3xl">
                   About
                 </Link>
               </Button>
 
               <Button asChild variant="ghost">
-                <Link href="/apidocs/">API</Link>
+                <Link href="/apidocs/" className="text-3xl">
+                  API
+                </Link>
               </Button>
 
               <Button asChild variant="ghost">
-                <Link href="/contribute/">Contribute</Link>
+                <Link href="/contribute/" className="text-3xl">
+                  Contribute
+                </Link>
               </Button>
             </div>
             <div className="min-h-screen">{children}</div>
